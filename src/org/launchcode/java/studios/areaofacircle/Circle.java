dd09566e39f035d7fc93a61bf67b5837f255c8fe
Micah <mrusher@gmail.com>
2021-05-05 16:58:32 -0500

    Finished first Studio without bonus

---
+++ src/org/launchcode/java/studios/areaofacircle/Circle.java
@@ -0,0 +1,9 @@
+package org.launchcode.java.studios.areaofacircle;
+
+import org.launchcode.java.studios.areaofacircle.Area;
+
+public class Circle {
+        public static Double getArea(Double radius) {
+            return 3.14 * radius * radius;
+        }
+}
