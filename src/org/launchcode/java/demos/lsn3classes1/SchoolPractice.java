60ead584daefe6578da2ad749a4f3dc32fe8c577
Micah <mrusher@gmail.com>
2021-05-17 16:27:12 -0500

    Student exercises done

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -1,7 +1,55 @@
 package org.launchcode.java.demos.lsn3classes1;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        Student student = new Student ();
+        student.setName("Micah");
+        student.setNumberOfCredits(1);
+        student.setGpa(4.0);
+    }
+}
+
+class Course{
+    private String topic;
+    private Teacher instructor;
+    private ArrayList<Student> enrolledStudents;
+}
+
+class Teacher{
+ private String firstName;
+ private String lastName;
+ private String subject;
+ private int yearsTeaching = 0;
+
+ public void setFirstName(String firstName){
+     this.firstName = firstName;
+    }
+
+ public void setLastName(String lastName){
+     this.lastName = lastName;
+ }
+
+ public void setSubject(String subject){
+     this.subject = subject;
+    }
+
+    public void setYearsTeaching(int yearsTeaching){
+     this.yearsTeaching = yearsTeaching;
+    }
+
+    public String getFirstName(){
+     return firstName;
+    }
+
+    public String getLastName(){
+     return lastName;
+    }
+
+    private int getYearsTeaching(){
+     return yearsTeaching;
     }
 }
---
+++ src/org/launchcode/java/studios/restaurantmenu/MenuItem.java
@@ -0,0 +1,6 @@
+package org.launchcode.java.studios.restaurantmenu;
+
+import java.util.Date;
+
+public class MenuItem {
+}
